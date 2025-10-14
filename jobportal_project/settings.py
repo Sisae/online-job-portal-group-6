5a1ef0cc523adcdca94c45c1f087500d3bc6e0f0
Floyd <dhliwayofloyd@gmail.com>
2025-10-14 11:11:53 +0200

    UI template updates

--- jobportal_project/settings.py
+++ jobportal_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     
     # Third party apps
     'rest_framework',
@@ -130,6 +131,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Optional (for local dev)
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Media files
 MEDIA_URL = '/media/'
